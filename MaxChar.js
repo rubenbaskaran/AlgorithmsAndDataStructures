ba457ce9812df6c44df2ce90da1d687cc8bd6283
Ruben Baskaran <rbaskaran92@gmail.com>
2018-05-18 12:47:23 +0200

    Handling edge cases

--- MaxChar.js
+++ MaxChar.js
@@ -1,10 +1,10 @@
 /* MaxChar */
 
-console.log(MaxChar("this is a test string"));
+console.log(MaxChar("aaabbbccc"));
 
 function MaxChar(inputString)
 {
-    var returnValue = ["", 0];
+    var returnObject = { "counter": 0, characters: [] };
 
     for (var character of inputString)
     {
@@ -22,11 +22,37 @@ function MaxChar(inputString)
             }
         }
 
-        if (counter > returnValue[1])
+        // If new MaxChar then empty array and add new MaxChar
+        if (counter > returnObject["counter"])
         {
-            returnValue = [character, counter];
-        }     
+            returnObject["counter"] = counter;
+            returnObject["characters"] = [];
+            returnObject["characters"].push(character);
+        }
+        // If equal MaxChar then add to array if it hasn't already been added
+        else if (counter == returnObject["counter"])
+        {
+            if (returnObject["characters"].indexOf(character) < 0)
+            {
+                returnObject["characters"].push(character);
+            }
+        }
     }
 
-    return "The most often occuring letter in '" + inputString + "' is " + returnValue[0];
+    // Concatenation of characters for output message
+    var outputString = "";
+    for (var char of returnObject["characters"])
+    {
+        if (outputString == "")
+        {
+            outputString = char;
+        }
+        else
+        {
+            outputString += " & " + char;
+        }
+    }
+
+    return "The most often occuring letter(s) in '" + inputString + "' is/are " +
+        outputString + ". It/they occur(s) " + returnObject["counter"] + " time(s)";
 }
