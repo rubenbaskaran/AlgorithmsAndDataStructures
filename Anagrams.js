3cdf1b7ae8b4c67267b66705c73415ff57a6505e
Ruben Baskaran <rbaskaran92@gmail.com>
2018-05-18 17:19:42 +0200

    Anagrams

---
+++ Anagrams.js
@@ -0,0 +1,67 @@
+/* Anagrams */
+
+console.log(Anagrams("!r!u!b!e!n!e!", " u b n e r e ! "));
+
+function Anagrams(stringOne, stringTwo)
+{
+    var characterCountingObjectOne = {};
+    var characterCountingObjectTwo = {};
+
+    // Create character counting object for stringOne
+    for (var character of stringOne)
+    {
+        if (!characterCountingObjectOne[character])
+        {
+            characterCountingObjectOne[character] = 1;
+        }
+        else
+        {
+            characterCountingObjectOne[character] = characterCountingObjectOne[character] + 1;
+        }
+    }
+
+    // Create character counting object for stringTwo
+    for (var character of stringTwo)
+    {
+        if (!characterCountingObjectTwo[character])
+        {
+            characterCountingObjectTwo[character] = 1;
+        }
+        else
+        {
+            characterCountingObjectTwo[character] = characterCountingObjectTwo[character] + 1;
+        }
+    }
+
+    // Compare character counting object for stringOne with equivalent for stringTwo
+    for (var character of stringOne)
+    {
+        // Ignore spaces and exclamation marks
+        if (character == " " || character == "!")
+        {
+            continue;
+        }
+
+        if (characterCountingObjectOne[character] != characterCountingObjectTwo[character])
+        {
+            return false;
+        }
+    }
+
+    // Compare character counting object for stringTwo with equivalent for stringOne
+    for (var character of stringTwo)
+    {
+        // Ignore spaces and exclamation marks
+        if (character == " " || character == "!")
+        {
+            continue;
+        }
+
+        if (characterCountingObjectTwo[character] != characterCountingObjectOne[character])
+        {
+            return false;
+        }
+    }
+
+    return true;
+}
