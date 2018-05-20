c6dca97f94da6c16d01a1ab530ecc75e1647b80b
Ruben Baskaran <rbaskaran92@gmail.com>
2018-05-20 14:16:26 +0200

    Added comments

--- Steps.js
+++ Steps.js
@@ -7,11 +7,13 @@ function Steps(numberOfSteps)
     var outputString = "";
     for (var i = 1; i < numberOfSteps + 1; i++)
     {
+        // Insert i numbers of "#" equal to the row number
         for (var j = 0; j < i; j++)
         {
             outputString += "#";
         }
 
+        // Fill out the remaining space with " " equal to the total space minus the inserted numbers of "#"
         for (var k = 0; k < numberOfSteps - i; k++)
         {
             outputString += " ";
