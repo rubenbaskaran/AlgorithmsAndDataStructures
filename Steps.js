59d8d30ce91d2b4bab5ed0cd56254e85c5a34b1c
Ruben Baskaran <rbaskaran92@gmail.com>
2018-05-20 14:12:22 +0200

    Refactored again

--- Steps.js
+++ Steps.js
@@ -5,7 +5,7 @@ Steps(10);
 function Steps(numberOfSteps)
 {
     var outputString = "";
-    for (var i = 1; i < numberOfSteps+1; i++)
+    for (var i = 1; i < numberOfSteps + 1; i++)
     {
         for (var j = 0; j < i; j++)
         {
