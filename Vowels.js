0c5d8dacd87f5fbec6f28ef03068a81f19d11e52
Ruben Baskaran <rbaskaran92@gmail.com>
2018-05-20 18:52:43 +0200

    Refactored

--- Vowels.js
+++ Vowels.js
@@ -4,9 +4,8 @@ Vowels("This Is A Test String");
 
 function Vowels(string)
 {
-    counter = 0;
-    var stringLowerCase = string.toLowerCase();
-    for (var char of stringLowerCase)
+    counter = 0;    
+    for (var char of string.toLowerCase())
     {
         if (char == "a" || char == "e" ||
             char == "i" || char == "o" || char == "u")
