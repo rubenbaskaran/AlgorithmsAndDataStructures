9531ce3e8d659174b94d1e5775a8ed4f3e08ccca
Ruben Baskaran <rbaskaran92@gmail.com>
2018-05-20 13:53:21 +0200

    Added Steps

---
+++ Steps.js
@@ -0,0 +1,23 @@
+/* Steps */
+
+Steps(10);
+
+function Steps(numberOfSteps)
+{
+    var outputString = "";
+    for (var i = 1; i < numberOfSteps+1; i++)
+    {
+        for (var j = 0; j < i; j++)
+        {
+            outputString == "" ? outputString = "#" : outputString += "#";
+        }
+
+        for (var k = 0; k < numberOfSteps - i; k++)
+        {
+            outputString == "" ? outputString = " " : outputString += " ";
+        }
+
+        console.log("'" + outputString + "'");
+        outputString = "";
+    }
+}
