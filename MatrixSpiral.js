e32a9bbae6186a9dc28c90c7a1c273095501a938
Ruben Baskaran <rbaskaran92@gmail.com>
2018-05-21 16:21:33 +0200

    Implemented MatrixSpiral

--- MatrixSpiral.js
+++ MatrixSpiral.js
@@ -9,18 +9,57 @@ function DrawMatrixSpiral(n)
     var startColumn = 0;
     var endColumn = n - 1;
     var endRow = n - 1;
+    var counter = 1;
+    var maxCounter = n * n;
 
     // Populate matrixArray with arrays
     for (var i = 0; i < n; i++)
     {
         matrixArray.push([]);
-    }    
+    }
 
-    // Horizontal to the right
+    while (counter <= maxCounter)
+    {
+        // Horizontal to the right
+        var x = startColumn;
+        while (x <= endColumn)
+        {
+            matrixArray[startRow][x] = counter;
+            counter++;
+            x++;
+        }
+        startRow++;
 
-    // Vertical to the bottom
+        // Vertical to the bottom
+        var y = startRow;
+        while (y <= endRow)
+        {
+            matrixArray[y][endRow] = counter;
+            counter++;
+            y++;
+        }
+        endColumn--;
 
-    // Horizontal to the left
+        // Horizontal to the left
+        var z = endColumn;
+        while (z >= startColumn)
+        {
+            matrixArray[endRow][z] = counter;
+            counter++;
+            z--;
+        }
+        endRow--;
 
-    // Vertical to the top
+        // Vertical to the top    
+        var v = endRow;
+        while (v >= startRow)
+        {
+            matrixArray[v][startColumn] = counter;
+            counter++;
+            v--;
+        }
+        startColumn++;
+    }
+
+    console.log(matrixArray);
 }
