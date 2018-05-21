123124ce4461697bf715c33993846667f6d0c48f
Ruben Baskaran <rbaskaran92@gmail.com>
2018-05-21 12:50:27 +0200

    MatrixSpiral initial commit

---
+++ MatrixSpiral.js
@@ -0,0 +1,26 @@
+/* MatrixSpiral */
+
+DrawMatrixSpiral(5);
+
+function DrawMatrixSpiral(n)
+{
+    var matrixArray = [];
+    var startRow = 0;
+    var startColumn = 0;
+    var endColumn = n - 1;
+    var endRow = n - 1;
+
+    // Populate matrixArray with arrays
+    for (var i = 0; i < n; i++)
+    {
+        matrixArray.push([]);
+    }    
+
+    // Horizontal to the right
+
+    // Vertical to the bottom
+
+    // Horizontal to the left
+
+    // Vertical to the top
+}
