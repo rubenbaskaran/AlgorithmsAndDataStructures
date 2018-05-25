ce688e4cd764a61a27963ee966c26c3919128015
Ruben Baskaran <rbaskaran92@gmail.com>
2018-05-25 11:50:45 +0200

    Weave and Peek for Queue

--- Queue.js
+++ Queue.js
@@ -14,17 +14,48 @@ class Queue
 
     Remove()
     {
-        this.queue.pop();
+        return this.queue.pop();
     }
 
     Print()
     {
         console.log(this.queue);
     }
+
+    Peek()
+    {
+        return this.queue[this.queue.length - 1];
+    }
+
+    Weave(queueOne, queueTwo)
+    {        
+        while (queueOne.Peek() || queueTwo.Peek())
+        {
+            if (queueOne.Peek())
+            {
+                this.Add(queueOne.Remove());
+            }
+
+            if (queueTwo.Peek())
+            {
+                this.Add(queueTwo.Remove());
+            }
+        }
+    }
 }
 
-var queue = new Queue();
-queue.Add(1);
-queue.Print();
-queue.Remove();
-queue.Print();
+var queueOne = new Queue();
+var queueTwo = new Queue();
+var mergedQueue = new Queue();
+
+queueOne.Add(1);
+queueOne.Add(2);
+queueOne.Add(3);
+queueTwo.Add("A");
+queueTwo.Add("B");
+queueTwo.Add("C");
+queueOne.Print();
+queueTwo.Print();
+
+mergedQueue.Weave(queueOne, queueTwo);
+mergedQueue.Print();
