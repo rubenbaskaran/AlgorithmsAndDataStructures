61351d4a700d424e9a606e0078e74653380f1b35
Ruben Baskaran <rbaskaran92@gmail.com>
2018-05-27 12:34:38 +0200

    Implemented OutOfBounce handling

--- BubbleSort.js
+++ BubbleSort.js
@@ -1,12 +1,13 @@
 /* BubbleSort */
 
-var numbers = [5,7,2,4,9,1,3,8,6];
+var numbers = [9,7,2,4,6,1,3,8,5];
 console.log(numbers);
 
 for(var i = 0; i < numbers.length; i++)
 {
-    for(var j = 0; j < numbers.length - i; j++)
+    for(var j = 0; j < numbers.length - i - 1; j++)
     {
+        var temp = numbers[j + 1];
         if(numbers[j] > numbers[j + 1])
         {
             smallNumber = numbers[j + 1];
