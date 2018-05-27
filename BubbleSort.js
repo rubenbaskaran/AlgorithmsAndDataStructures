1259e7a48015e5baba1d71bbb49709e920233202
Ruben Baskaran <rbaskaran92@gmail.com>
2018-05-27 12:36:29 +0200

    Removed redundant line

--- BubbleSort.js
+++ BubbleSort.js
@@ -1,14 +1,13 @@
 /* BubbleSort */
 
-var numbers = [9,7,2,4,6,1,3,8,5];
+var numbers = [9, 7, 2, 4, 6, 1, 3, 8, 5];
 console.log(numbers);
 
-for(var i = 0; i < numbers.length; i++)
+for (var i = 0; i < numbers.length; i++)
 {
-    for(var j = 0; j < numbers.length - i - 1; j++)
+    for (var j = 0; j < numbers.length - i - 1; j++)
     {
-        var temp = numbers[j + 1];
-        if(numbers[j] > numbers[j + 1])
+        if (numbers[j] > numbers[j + 1])
         {
             smallNumber = numbers[j + 1];
             bigNumber = numbers[j];
